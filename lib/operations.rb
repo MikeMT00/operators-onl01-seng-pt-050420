a7714646e149e198c096d50f9ba39c88275a92a2
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-06 18:56:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ end
 
 
 def not_safe?(speed)
-	speed < 40 ? false : false
-	speed > 60 ? false : false
+	speed == speed < 40 ? false : false
+	speed == speed > 60 ? false : false
 	speed = 40 && 60 ? false : true
 end
